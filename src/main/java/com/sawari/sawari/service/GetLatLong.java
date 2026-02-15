a551c607842e5b79dbe17ab4017bf1950deb9d44
avishekGit2024 <patraavishek881@gmail.com>
2026-02-15 23:27:34 +0530

    redis confuration for location-geodetails

--- src/main/java/com/sawari/sawari/service/GetLatLong.java
+++ src/main/java/com/sawari/sawari/service/GetLatLong.java
@@ -1,18 +1,36 @@
 package com.sawari.sawari.service;
 import com.sawari.sawari.pojo.Geocode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @Slf4j
 public class GetLatLong {
+    @Autowired
+    private RedisService redisService;
+
     private static String LocationIQApiKey = "pk.7d3dc7f86db9c8b5c9adbe39d87df682";  //need to set the Api key in env
     private static final String Url = "https://us1.locationiq.com/v1/";
     public ResponseEntity<String> GetLatAndLongForLocation(String Location){
         try{
+
+            //now the application flow will be
+            String loc = Location;
+            loc=loc.trim().toLowerCase();
+            //->first check the location exist in redis
+            Geocode fromRedisGeocode = redisService.getFromRedis(loc);
+            //->if yes then return it directly
+            if(fromRedisGeocode!=null){
+                System.out.println("Hey i Got it from redis");
+                return new ResponseEntity<>(fromRedisGeocode.getDisplayName(), HttpStatus.OK);
+            }
+            //->if not then fetch it and store it in redis and return it
             String uri = String.format(
                     "%ssearch?key=%s&q=%s&format=json&",
                     Url,
@@ -21,9 +39,8 @@ public class GetLatLong {
             );
             RestTemplate restTemplate = new RestTemplate();
             Geocode[] response = restTemplate.getForObject(uri, Geocode[].class);
-//            todo remove just for checking
-            System.out.println(response[0]);
-            return new ResponseEntity<>(response[0].getLat()+response[0].getLon(), HttpStatus.OK);
+            redisService.setToRedis(loc,response[0],24, TimeUnit.HOURS);
+            return new ResponseEntity<>(response[0].getDisplayName(), HttpStatus.OK);
         }catch (Exception e){
             log.error("Failed to get Geocode",e.getMessage());
             return new ResponseEntity<>("Failed to get Geocode",HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/com/sawari/sawari/service/RedisService.java
+++ src/main/java/com/sawari/sawari/service/RedisService.java
@@ -1,4 +1,32 @@
 package com.sawari.sawari.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import com.sawari.sawari.pojo.Geocode;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
 public class RedisService {
+    @Autowired
+    private RedisTemplate<String, Geocode> redisTemplateForGeocode;
+
+    public void setToRedis(String key, Geocode value, long ttl, TimeUnit timeUnit){
+        try{
+            System.out.println("Set to redis "+key);
+            redisTemplateForGeocode.opsForValue().set(key, value, ttl, timeUnit);
+        }catch (Exception e){
+            log.error("Failed to set key {} in Redis", e.getMessage());
+        }
+    }
+    public Geocode getFromRedis(String key){
+        try{
+             return redisTemplateForGeocode.opsForValue().get(key);
+        }catch (Exception e){
+            log.error("Failed to get key {} in Redis", e.getMessage());
+            return null;
+        }
+    }
 }
