2f3d79fa476b20dbe23bf56196efb83da4eb11ed
avishekGit2024 <patraavishek881@gmail.com>
2026-03-11 15:12:28 +0530

    controller for route based on src and dest and in GetDirectionsLIq.java removed the hard coded lat<- long to request body lat&long of(src,det

--- src/main/java/com/sawari/sawari/controller/GetGeoDetailsOfLocationAndDirectionController.java
+++ src/main/java/com/sawari/sawari/controller/GetGeoDetailsOfLocationAndDirectionController.java
@@ -5,19 +5,19 @@ import com.sawari.sawari.pojo.SourceDestinationLatAndLong;
 import com.sawari.sawari.service.GetDirectionsLIq;
 import com.sawari.sawari.service.GetLatLong;
 import com.sawari.sawari.service.GetSuggestionServiceForLocation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/get")
-public class GetGeoDetailsOfLocationAndDirection {
+public class GetGeoDetailsOfLocationAndDirectionController {
     @Autowired
     private GetLatLong getLatLong;
 
@@ -36,8 +36,14 @@ public class GetGeoDetailsOfLocationAndDirection {
         return getSuggestionServiceForLocation.getSuggestion(location);
     }
 
-    @GetMapping("/src_dest/direction")
+    @PostMapping("/src_dest/direction")
     public ResponseEntity<?>getDirectionWithGeometry(@RequestBody SourceDestinationLatAndLong body){
-
+        try {
+            DirectionResponse response = getDirectionsLIq.getDirections(body);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/sawari/sawari/service/GetDirectionsLIq.java
+++ src/main/java/com/sawari/sawari/service/GetDirectionsLIq.java
@@ -2,12 +2,10 @@ package com.sawari.sawari.service;
 
 import com.sawari.sawari.helper.StringFormatter;
 import com.sawari.sawari.pojo.DirectionResponse;
-import com.sawari.sawari.pojo.Route;
+import com.sawari.sawari.pojo.SourceDestinationLatAndLong;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,20 +15,21 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class GetDirectionsLIq {
     @Value("${LocationIq.account.api}")
-    private String LocationIQApiKey; //todo
+    private String LocationIQApiKey;
 
     @Autowired
     private StringFormatter stringFormatter;
     @Autowired
     private RedisService redisService;
+    @Autowired
+    private RestTemplate restTemplate;
     private static final String Url = "https://us1.locationiq.com/v1/directions/";
-    //hardcoded need to update
-    private Double srcLat = 51.514156;
-    private Double srcLon = -0.12070277;
-    private Double dstLat = 51.507996;
-    private Double dstLon = -0.12360937;
-    public ResponseEntity<DirectionResponse> getDirections() {
-        try{
+
+    public DirectionResponse getDirections(SourceDestinationLatAndLong body) {
+            Double srcLon = Double.parseDouble(body.getSourceLongitude());
+            Double srcLat = Double.parseDouble(body.getSourceLatitude());
+            Double dstLon = Double.parseDouble(body.getDestinationLongitude());
+            Double dstLat = Double.parseDouble(body.getDestinationLatitude());
             String uri = String.format(
                     "%sdriving/%f,%f;%f,%f?key=%s&steps=true&alternatives=true&geometries=polyline&overview=full",
                     Url,
@@ -40,19 +39,15 @@ public class GetDirectionsLIq {
                     dstLat,
                     LocationIQApiKey
             );
-            String redisKeyForSrcDestGeocode = stringFormatter.FormatStringForSrcDestGeocode(srcLon, srcLat, dstLon, dstLat);
-            DirectionResponse GeometryFromRedis = redisService.getFromRedisGeometry(redisKeyForSrcDestGeocode);
+            //first check in redis of present return it
+            String redisKeyForSrcDestinationDirection = stringFormatter.FormatStringForSrcDestGeocode(srcLon, srcLat, dstLon, dstLat);
+            DirectionResponse GeometryFromRedis = redisService.getFromRedisGeometry(redisKeyForSrcDestinationDirection);
             if(GeometryFromRedis != null){
-                System.out.println("get it from redis");
-                return new ResponseEntity<>(GeometryFromRedis, HttpStatus.OK);
+                return GeometryFromRedis;
             }
-            RestTemplate restTemplate = new RestTemplate();
             DirectionResponse response =  restTemplate.getForObject(uri, DirectionResponse.class);
-            redisService.SetToRedisGeometry(redisKeyForSrcDestGeocode,response,24, TimeUnit.HOURS);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }catch(Exception e){
-            log.error("Error while fetching directions", e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+            //first time set to redis
+            redisService.SetToRedisGeometry(redisKeyForSrcDestinationDirection,response,24, TimeUnit.HOURS);
+            return response;
     }
 }
