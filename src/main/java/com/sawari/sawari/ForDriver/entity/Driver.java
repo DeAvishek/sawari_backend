0c9906977e9864a653c78cdba1977736f0fa8cd7
avishekGit2024 <patraavishek881@gmail.com>
2026-03-12 09:52:07 +0530

    separate driver entity and location entity of driver one<->one mapping

---
+++ src/main/java/com/sawari/sawari/ForDriver/entity/Driver.java
@@ -0,0 +1,32 @@
+package com.sawari.sawari.ForDriver.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "DriverId")
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false,length = 10)
+    private String phoneNumber;
+
+    @Column(nullable = false,length =6)
+    private String Otp;
+
+    private Boolean isVerified;
+    private Boolean isOnline;
+    private Boolean isOnRide;
+
+    //private location other entity map to one <-> one with driver
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
+    private DriverLocation driverLocation;
+}
